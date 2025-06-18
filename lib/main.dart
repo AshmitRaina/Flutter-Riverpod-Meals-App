0e6bd55a83861b84f6fcb8fabc514a3df6acd4b9
Ashmit Raina <ashmitraina82@gmail.com>
2025-06-18 23:34:55 +0530

    device preview added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/tab_screen.dart';
@@ -11,13 +13,21 @@ final theme = ThemeData(
   textTheme: GoogleFonts.latoTextTheme(),
 );
 void main() {
-  runApp(const App());
+  runApp(
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const App()),
+  );
 }
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const TabScreen());
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      theme: theme,
+      home: const TabScreen(),
+    );
   }
 }
